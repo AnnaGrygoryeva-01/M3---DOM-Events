dc65ee5f09c9a75687eec11f4b61f20202142401
Anna Grygoryeva <annagrygoryeva3101@gmail.com>
2026-03-23 21:06:19 +0000

    Fix mistakes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // 1. Прописати у html-розмітці кнопку (<button>) з будь-яким селектором (класс, id). За допомогою DOM отримати посилання на цей елемент та навісити на нього обробник події кліку. За кліком кнопка має викликати модальне вікно (alert) з текстом “Привіт тобі, клацальщик!”
 
-const clickMeBtn = document.querySelector("#clickMeBtn");
+const clickMeBtn = document.getElementById("clickMeBtn");
 
-function clickMeBtnHandler(e) {
+function clickMeBtnHandler() {
   alert("Привіт тобі, клацальщик!");
 }
 clickMeBtn.addEventListener("click", clickMeBtnHandler);
 
 // 2. Створити посилання з текстом “клікни, аби з’явилась кнопка”. За натиснення на посилання поряд з ним має з’явитись новий елемент - кнопка.
 
-const createBtnLink = document.querySelector("#createBtnLink");
+const createBtnLink = document.getElementById("createBtnLink");
 
-function createBtnHandler(e) {
-  const hiddenBtn = document.querySelector("#hiddenBtn");
+function createBtnHandler() {
+  const hiddenBtn = document.getElementById("hiddenBtn");
   hiddenBtn.hidden = false;
 }
 createBtnLink.addEventListener("click", createBtnHandler);
@@ -28,10 +28,10 @@ createBtnLink.addEventListener("click", createBtnHandler);
 
 // 3. “Лампочка”. У розмітці прописати елемент (article або div), з початковими стилями, які роблять елемент круглим, сірого кольору тла, з темно-сірою рамкою. Також прописати в розмітці кнопку, за натиснення на яку у елемента-лампочки мають змінитись стилі - тло має стати жовтим, рамка - білою.
 
-const lightbulb = document.querySelector("#lightbulb");
-const toggleLightBtn = document.querySelector("#toggleLightBtn");
+const lightbulb = document.getElementById("lightbulb");
+const toggleLightBtn = document.getElementById("toggleLightBtn");
 
-function toggleLightHandler(e) {
+function toggleLightHandler() {
   lightbulb.classList.toggle("bulb-on");
 }
 
